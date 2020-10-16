d18c180710c21d2925addaf3011a6e2fa446e82a
tuduraebira <tuduraebira@gmail.com>
2020-10-17 01:36:17 +0900

    test10

--- test.js
+++ test.js
@@ -1,19 +1,5 @@
-$(function(){
-    //.sampleをクリックしてajax通信を行う
-    $('.sample_btn').click(function(){
-        $.ajax({
-            url: '/ajax/test.html',
-            /* 自サイトのドメインであれば、https://kinocolog.com/ajax/test.html というURL指定も可 */
-            type: 'GET',
-            dataType: 'html'
-        }).done(function(data){
-            /* 通信成功時 */
-            $('.result').html(data); //取得したHTMLを.resultに反映
-            
-        }).fail(function(data){
-            /* 通信失敗時 */
-            alert('通信失敗！');
-                    
-        });
-    });
-});
+let str = 'hoge';
+
+javascript:(function () {
+    alert(str);
+})();
