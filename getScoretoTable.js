1420223880b57965f1120142668ae643ee935d96
tuduraebira <tuduraebira@gmail.com>
2020-10-20 21:42:10 +0900

    [delete]コメントを削除
    コメントアウトしていたものを削除

--- getScoretoTable.js
+++ getScoretoTable.js
@@ -6,69 +6,6 @@ let url_part = [];
 let chart_data = [];
 let max_page = -1;
 
-$.wait = (function(msec){
-    let d = $.Deferred();
-    setTimeout(function(){
-        d.resolve();
-    }, msec);
-    return d.promise();
-});
-
-/*const getURLData = (function(page){
-    $.wait(5000).done(function(){
-        $.ajax({
-            type: 'GET',
-            url: BASE_URL + MUSIC_URL + '?page=' + String(page) + '&sort=0'
-        }).done(function(data_url){
-            if($(data_url).find('.data_col').length <= 0){
-                setTimeout(function(){
-                    $.ajax(this);
-                }, 500);
-            }
-
-            $(data_url).find('.data_col').each(function(){
-                let url_music = $(this).find('.music .title a').attr('href');
-
-                if(url_music != null){
-                    //getScoreData(url_music);
-                    url_part.push(url_music);
-                }else{
-                    alert('取得失敗');
-                    return false;
-                }
-
-                console.log(url_part);
-            });
-        }).fail(function(){
-            setTimeout(function(){
-                $.ajax(this);
-            }, 500);
-        });
-    });
-});*/
-
-/*const getScoreData = (function(url_m){
-    txt = url_m;
-    $.ajax({
-        type: 'GET',
-        url: BASE_URL + url_m
-    }).done(function(data_score){
-        txt = 'success';
-        console.log(txt);
-        $(data_score).find('.level').each(function(){
-            txt = $(this).text();
-        });
-        return;
-    }).fail(function(){
-        txt = 'failed';
-        console.log(txt);
-        setTimeout(function(){
-            $.ajax(this);
-        }, 500);
-    });
-    console.log('end');
-});*/
-
 alert('プレースコアを取得します。\n※公式サイトのプロフィール画面から起動させてください。');
 
 const bg_tool = $('<div>').addClass('bg_tool').attr('style', 'position:fixed; top:0; z-index:10; width:100%; height:100%; background-color:rgba(0,0,0,0.6);');
@@ -77,30 +14,6 @@ const txt_getScore = $('<span>').addClass('txt_getScore').attr('style', 'font-si
 box_info.append(txt_getScore);
 bg_tool.append(box_info);
 $('body').append(bg_tool);
-
-/*$.ajax({
-    type: 'GET',
-    url: BASE_URL + MUSIC_URL
-}).done(function(data_num){
-    max_page = Number($(data_num).find('.page_num').last().text());
-    if(max_page == 0){
-        alert('スコアを取得できませんでした。');
-        return;
-    }
-
-    for(let i = 1; i <= max_page; i++){
-        getURLData(i);
-    }
-    console.log('show the text');
-
-    //alert('接続成功です。');
-    //return;
-
-    $.wait((max_page + 20) * 1000).done(function(){
-        alert('通信品質が悪いです。');
-        return;
-    })
-})*/
 
 function getMaxPage(){
     return new Promise((resolve, reject) => {
@@ -120,13 +33,7 @@ function getMaxPage(){
                     alert('スコアを取得できませんでした。');
                     throw new Error('failed to get score');
                 }
-        
-                /*$.wait((max_page + 20) * 1000).done(function(){
-                    alert('通信品質が悪いです。');
-                    throw new Error('cannot connected');
-                });*/
 
-                console.log('1:' + max_page);
                 resolve('ページ数取得完了');
             });
         }catch(err){
@@ -149,8 +56,6 @@ function getURLPartData(page){
                 }
             }).done(function(data_url){
                 if($(data_url).find('.data_col').length <= 0){
-                    //console.log(page);
-                    //throw new Error('cannot find data column');
                     resolve('URLデータ取得完了');
                 }
 
@@ -164,7 +69,6 @@ function getURLPartData(page){
                     }
                 });
 
-                //console.log(url_part);
                 resolve('URLデータ取得完了');
             });
         }catch(err){
@@ -221,8 +125,9 @@ function exportCSVFile(){
             const blob = new Blob([ bom, chart_data.join('\r\n') ], { type : 'text/csv' });
             const element = document.createElement('a');
             const objectUrl = window.URL.createObjectURL(blob);
+
             element.href = objectUrl;
-            element.setAttribute('download', 'score_data'); 
+            element.setAttribute('download', 'score_data.csv'); 
             document.body.appendChild(element);
             element.click();
             window.URL.revokeObjectURL(objectUrl);
@@ -235,19 +140,6 @@ function exportCSVFile(){
         }
     });
 }
-
-/*export const downloadCSV = (rows, fileName = 'download') => { 
-    const bom = new Uint8Array([0xEF, 0xBB, 0xBF]) 
-    const blob = new Blob([ bom, rows.join('\r\n') ], { type : 'text/csv' }) 
-    const element = document.createElement('a') 
-    const objectUrl = window.URL.createObjectURL(blob)
-    element.href = objectUrl 
-    element.setAttribute('download', fileName); 
-    document.body.appendChild(element) 
-    element.click() 
-    window.URL.revokeObjectURL(objectUrl) 
-    document.body.removeChild(element) 
-} */
 
 async function processAll(){
     try{
